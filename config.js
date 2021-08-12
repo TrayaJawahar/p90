32519945879992704e4cfa634462cf2a7dc68a0f
unknown <trayasai2006@gmail.com>
2021-08-12 11:08:03 +0530

    project

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyD_pofj5o9TFLcbsddx_UzV3YcP29JfW1E',
+    authDomain: 'story-telling-app-9caa4.firebaseapp.com',
+    databaseURL: 'https://story-telling-app-9caa4-default-rtdb.firebaseio.com',
+    projectId: 'story-telling-app-9caa4',
+    storageBucket: 'story-telling-app-9caa4.appspot.com',
+    messagingSenderId: '531177969740',
+    appId: '1:531177969740:web:5964a2d889fa807100fce9',
+  };
+  
